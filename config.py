bfe22751d423f253dcec8c195a7ab2746615af64
plurovarivant-arch <plurovarivant@gmail.com>
2026-07-06 18:57:15 +0200

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.getenv("8768438638:AAGWgS28BfiAwkdhxMzXPKY9e4uIu3j9fM0")
 
 COMPANY_INFO = {
     'name': 'FreaksServis',
